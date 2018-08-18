c620b9ed4ef24e2c9e38c59459c82ad20302ba7c
Samuel Cherukutty Cheruvathur <samuel.cc@zohocorp.com>
2018-08-18 11:05:05 +0530

    Fetch functions done

--- include/sudoku.hh
+++ include/sudoku.hh
@@ -2,18 +2,23 @@
 #define SUDOKU_HH
 #include <array>
 #include <iostream>
-#include <vector>
-using std::vector, std ::array;
+#include <set>
+#define MAT_SIZE 9
+#define TOTAL_ENTRIES (MAT_SIZE * MAT_SIZE)
+using std ::array, std::set;
 
 class Sudoku {
 private:
-  array<array<short, 9>, 9> matrix;
-  array<vector<short>, 81> possibilities;
+  array<array<short, MAT_SIZE>, MAT_SIZE> matrix;
+  array<set<short>, TOTAL_ENTRIES> possibilities;
 
 public:
   int solve();
   int fetchMatrix();
   int printMatrix();
+  set<short> fetchHorizontal(short row);
+  set<short> fetchVertical(short column);
+  set<short> fetchSquare(short row, short column);
   int updatePossibilities();
 };
 #endif // !SUDOKU_HH
--- src/sudoku.cc
+++ src/sudoku.cc
@@ -1,6 +1,7 @@
 #include "sudoku.hh"
 #include <iostream>
 using std::string, std::cin, std::cout;
+
 int Sudoku ::fetchMatrix() {
   int line; /* Each line is read as a integer and loaded to matrix */
   for (int i = 0; i < 9; i++) {
@@ -17,16 +18,81 @@ int Sudoku ::fetchMatrix() {
 
 int Sudoku::printMatrix() {
   for (int i = 0; i < 9; i++) {
+    cout << "\n-------------------\n|";
     for (int j = 0; j < 9; j++) {
-      cout << matrix[i][j] << " ";
+      cout << matrix[i][j] << "|";
     }
-    cout << "\n";
+  }
+  cout << "\n-------------------\n";
+  return 0;
+}
+
+int Sudoku::updatePossibilities() {
+  for (int i = 0; i < 9; i++) {
+    for (int j = 0; j < 9; j++) {
+    }
   }
   return 0;
 }
+
+set<short> Sudoku::fetchHorizontal(short row) {
+  set<short> returnSet;
+  for (short column = 0; column < MAT_SIZE; column++) {
+    short value = matrix[row][column];
+    if (value != 0) {
+      returnSet.insert(value);
+    }
+  }
+  return returnSet;
+}
+
+set<short> Sudoku::fetchVertical(short column) {
+  set<short> returnSet;
+  for (short row = 0; row < MAT_SIZE; row++) {
+    short value = matrix[row][column];
+    if (value != 0) {
+      returnSet.insert(value);
+    }
+  }
+  return returnSet;
+}
+
+set<short> Sudoku::fetchSquare(short row, short column) {
+  set<short> returnSet;
+  short sectionMinRowOffset = (row / 3) * 3;
+  short sectionMinColumnOffset = (row / 3) * 3;
+  short sectionMaxRowOffset = sectionMinRowOffset + 3;
+  short sectionMaxColumnOffset = sectionMinColumnOffset + 3;
+
+  for (short sectionRow = sectionMinRowOffset; sectionRow < sectionMaxRowOffset;
+       sectionRow++) {
+    for (short sectionColumn = sectionMinColumnOffset;
+         sectionColumn < sectionMaxColumnOffset; sectionColumn++) {
+      short value = matrix[sectionRow][sectionColumn];
+      if (value != 0) {
+        returnSet.insert(value);
+      }
+    }
+  }
+  return returnSet;
+}
+
+void printSet(set<short> inputSet) {
+  for (auto i : inputSet) {
+    cout << i << " ";
+  }
+}
+
 int main() {
   Sudoku sudoku;
   sudoku.fetchMatrix();
   sudoku.printMatrix();
+  cout << "\n";
+  printSet(sudoku.fetchHorizontal(2));
+  cout << "\n";
+  printSet(sudoku.fetchVertical(5));
+  cout << "\n";
+  printSet(sudoku.fetchSquare(5, 5));
+  cout << "\n";
   return 0;
 }
